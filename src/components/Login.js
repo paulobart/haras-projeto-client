e2ab5bfc7006deb5a7af31296b82198a6efae9aa
paulobart <paulobart@outlook.com>
2021-04-16 18:25:52 +0100

    Página de Login finalizada, componente login no app.js, mudança de cor no botão da Navbar

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,55 +2,62 @@ import React, { Component } from "react";
 import apiUtils from "../api/api.utils";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-    state = {
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await apiUtils.login(this.state);
+      this.setState({
         email: "",
-        password:""
+        password: "",
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            await apiUtils.login(this.state);
-            this.setState({
-                email: "",
-                password:""
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    handleInput = (event) => {
-        const {name, value } = event.target
-        this.setState({
-            [name]: value
-        })
-    }
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
     return (
       <div>
-        <div className="field">
-          <p className="control has-icons-left has-icons-right">
-            <input className="input" value={this.state.email} type="email" placeholder="Email" name='email' onChange={this.handleInput} />
-            <span className="icon is-small is-left">
-              <i className="fas fa-envelope"></i>
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check"></i>
-            </span>
-          </p>
-        </div>
-        <div className="field">
-          <p className="control has-icons-left">
-          <input className="input" value={this.state.password} type="password" placeholder="Password" name='password' onChange={this.handleInput} />
-            <span className="icon is-small is-left">
-              <i className="fas fa-lock"></i>
-            </span>
-          </p>
-          <button className="button" onClick={this.handleSubmit}>Login</button>
-        </div>
+        <form className="box" style={{ marginRight: "20%", marginLeft: "20%" }}>
+          <div className="container">
+            <div className="notification is-fluid">
+              <div className="field">
+                <label className="label">Login</label>
+                <div className="control">
+                  <input className="input" value={this.state.email} type="email" placeholder="e.g. alex@example.com" name="email" onChange={this.handleInput}/>
+                </div>
+              </div>
+
+              <div className="field">
+                <label className="label"></label>
+                <div className="control">
+                  <input className="input" value={this.state.password} type="password" placeholder="********" name="password" onChange={this.handleInput}/>
+                </div>
+              </div>
+              <button className="button is-fullwidth" className="button is-info" onClick={this.handleSubmit}>
+                <p>Login</p>
+              </button>
+            </div>
+            <div className="notification is-fluid">
+              <div className="field"></div>
+              <label className="label">Ou crie a sua conta</label>
+              <div className="control"></div>
+              <button className="button is-fullwidth" className="button is-info" onClick={this.handleSubmit}>Sign up</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
